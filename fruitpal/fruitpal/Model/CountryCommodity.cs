189d1ba6a17f6e359e15e648a1fa053d69a65cdf
Chris Martin <martin5408@gmail.com>
2020-03-06 21:43:40 -0600

    Setting up project structure

---
+++ fruitpal/fruitpal/Model/CountryCommodity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fruitpal.Model
+{
+    class CountryCommodity
+    {
+    }
+}
---
+++ fruitpal/fruitpalTests/LogicTests/FruitCommodityLogicTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fruitpalTests.LogicTests
+{
+    class FruitCommodityLogicTests
+    {
+    }
+}
